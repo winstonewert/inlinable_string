de96a7228afd9b0c1ee6e9f5a0b8cb25982df765
Travis CI User <travis@example.org>
2017-09-26 17:36:27 +0000

    Documentation for fitzgen/inlinable_string@b3fbcdd

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["inlinable_string"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"inlinable_string/inline_string/struct.InlineString.html\" title=\"struct inlinable_string::inline_string::InlineString\">InlineString</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"inlinable_string/inline_string/struct.NotEnoughSpaceError.html\" title=\"struct inlinable_string::inline_string::NotEnoughSpaceError\">NotEnoughSpaceError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"inlinable_string/enum.InlinableString.html\" title=\"enum inlinable_string::InlinableString\">InlinableString</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/iter/traits/trait.Extend.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["inlinable_string"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.char.html\">char</a>&gt; for <a class=\"enum\" href=\"inlinable_string/enum.InlinableString.html\" title=\"enum inlinable_string::InlinableString\">InlinableString</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;&amp;'a <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.char.html\">char</a>&gt; for <a class=\"enum\" href=\"inlinable_string/enum.InlinableString.html\" title=\"enum inlinable_string::InlinableString\">InlinableString</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;&amp;'a <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"inlinable_string/enum.InlinableString.html\" title=\"enum inlinable_string::InlinableString\">InlinableString</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
